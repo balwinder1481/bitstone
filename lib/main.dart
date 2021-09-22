73145d3a5be28404b7015278fe929cba4d7f7607
Balwinder <balwinder1481@gmail.com>
2021-09-22 17:43:25 +0530

    Day 3 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,29 @@
-import 'package:bitstone/home_page.dart';
+import 'package:bitstone/pages/home_page.dart';
+import 'package:bitstone/pages/login_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      darkTheme: ThemeData(
+      brightness: Brightness.dark,
+      ),
+      initialRoute: "/home",
+      routes: {
+        "/":(context)=> const LoginPage(),
+        "/home":(context)=> HomePage(),
+        "/": (context) => const LoginPage()
+      },
     );
   }
 }
