fa0cdbd41543f58433b5375c81331f3c0e7763c7
Balwinder <balwinder1481@gmail.com>
2021-09-23 17:13:05 +0530

    token veiw implememted

--- lib/models/market.dart
+++ lib/models/market.dart
@@ -1,6 +1,8 @@
+import 'dart:convert';
+
 
 class MarketModel {
- static final tokens = [
+ static List<Token> tokens = [
    Token(
      id: 1, 
      name: "BitStone", 
@@ -17,7 +19,79 @@ class Token {
   final num price;
   final String image;
 
-  Token({required this.id, required this.name, required this.symbol, required this.price, required this.image});
+  Token({
+    required this.id,
+    required this.name,
+    required this.symbol,
+    required this.price,
+    required this.image,
+  });
 
   
+
+  Token copyWith({
+    int? id,
+    String? name,
+    String? symbol,
+    num? price,
+    String? image,
+  }) {
+    return Token(
+      id: id ?? this.id,
+      name: name ?? this.name,
+      symbol: symbol ?? this.symbol,
+      price: price ?? this.price,
+      image: image ?? this.image,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'name': name,
+      'symbol': symbol,
+      'price': price,
+      'image': image,
+    };
+  }
+
+  factory Token.fromMap(Map<String, dynamic> map) {
+    return Token(
+      id: map['id'],
+      name: map['name'],
+      symbol: map['symbol'],
+      price: map['price'],
+      image: map['image'],
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory Token.fromJson(String source) => Token.fromMap(json.decode(source));
+
+  @override
+  String toString() {
+    return 'Token(id: $id, name: $name, symbol: $symbol, price: $price, image: $image)';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+  
+    return other is Token &&
+      other.id == id &&
+      other.name == name &&
+      other.symbol == symbol &&
+      other.price == price &&
+      other.image == image;
+  }
+
+  @override
+  int get hashCode {
+    return id.hashCode ^
+      name.hashCode ^
+      symbol.hashCode ^
+      price.hashCode ^
+      image.hashCode;
+  }
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,16 +2,35 @@ import 'package:bitstone/models/market.dart';
 import 'package:bitstone/widgets/drawer.dart';
 import 'package:bitstone/widgets/token_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:convert';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    var marketJson = await rootBundle.loadString("assets/files/market.json");
+    var decodedData = jsonDecode(marketJson);
+    var tokensData = decodedData["tokens"];
+    MarketModel.tokens = List.from(tokensData)
+    .map<Token>((token) => Token.fromMap(token))
+    .toList();
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
-
-    final dummyList = List.generate(4, (index) => MarketModel.tokens[0]);
-
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.white,
@@ -22,10 +41,10 @@ class HomePage extends StatelessWidget {
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: ListView.builder(
-            itemCount: dummyList.length,
+            itemCount: MarketModel.tokens.length,
             itemBuilder: (context, index) {
               return TokenWidget(
-                token: dummyList[index],
+                token: MarketModel.tokens[index],
               );
             }),
       ),
